8317130470b06e61645d6e9785c09a54d5d17b89
George123321 <gosha-dem@ya.ru>
2017-10-01 18:05:05 +0300

    Синхр.

--- Class Numeric/test.py
+++ Class Numeric/test.py
@@ -2,7 +2,9 @@ from Class_Numeric import Numeric
 from Class_Matrix import Matrix
 from Class_Matrix import Identity_Matrix
 
-A = Matrix(3, 4)
+A = Matrix(3)
 A.write_matrix()
-A.gauss()
-A.print()
+A.invert_matrix().print()
+B = A.clip(Identity_Matrix(A.n))
+B.gauss()
+B.print()
